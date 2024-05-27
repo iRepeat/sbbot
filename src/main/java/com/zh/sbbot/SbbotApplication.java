ff15fb7ae994e5d5e079001d872e2002eba92280
hao.zhao <sanguoqjuan@qq.com>
2024-05-27 15:30:10 +0800

    -- init project

---
+++ src/main/java/com/zh/sbbot/SbbotApplication.java
@@ -0,0 +1,13 @@
+package com.zh.sbbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zh/sbbot/SbbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zh.sbbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
