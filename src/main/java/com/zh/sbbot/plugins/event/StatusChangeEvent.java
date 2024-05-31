1936f30c3e9a418ee69452aad42132f80a307f5e
hao.zhao <sanguoqjuan@qq.com>
2024-05-31 13:11:33 +0800

    🚨：增加BOT状态变更事件通知

---
+++ src/main/java/com/zh/sbbot/plugins/event/StatusChangeEvent.java
@@ -0,0 +1,36 @@
+package com.zh.sbbot.plugins.event;
+
+import com.mikuac.shiro.core.Bot;
+import com.mikuac.shiro.core.CoreEvent;
+import com.zh.sbbot.configs.SystemSetting;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+
+@Primary
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class StatusChangeEvent extends CoreEvent {
+    private final SystemSetting systemSetting;
+
+    @Override
+    public void online(Bot bot) {
+        log.info("bot已上线：{}", bot.getSelfId());
+        systemSetting.getSuperUser().forEach(userId -> bot.sendPrivateMsg(userId, "我上线啦～", false));
+    }
+
+    @Override
+    public void offline(long account) {
+        log.info("bot已下线：{}", account);
+    }
+
+    @Override
+    public boolean session(WebSocketSession session) {
+        // 返回 false 即可禁止连接
+        return true;
+    }
+
+}
