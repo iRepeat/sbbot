8c7c9d337ff057348ad88c14115b56a5df6716ac
hao.zhao <sanguoqjuan@qq.com>
2024-08-16 10:49:05 +0800

    🥩：AI增强

--- src/main/java/com/zh/sbbot/plugin/ai/AiPlugin.java
+++ src/main/java/com/zh/sbbot/plugin/ai/AiPlugin.java
@@ -18,18 +18,18 @@ import com.zh.sbbot.plugin.ai.dao.PluginAiRepository;
 import com.zh.sbbot.plugin.ai.handler.AiHandler;
 import com.zh.sbbot.plugin.ai.handler.AiHandlerSelector;
 import com.zh.sbbot.plugin.ai.support.ChatResponse;
-import com.zh.sbbot.util.BotHelper;
-import com.zh.sbbot.util.BotUtil;
-import com.zh.sbbot.util.OCRUtil;
-import com.zh.sbbot.util.TTSUtil;
+import com.zh.sbbot.util.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Objects;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import static com.zh.sbbot.util.BotUtil.getText;
@@ -53,67 +53,37 @@ public class AiPlugin {
         Long groupId = event.getGroupId();
 
         // 读取当前群组的配置
-        PluginAi pluginAi = pluginAiRepository.findOne(groupId);
-        if (pluginAi == null) {
-            log.error("AI配置未初始化：{}", groupId);
-            return;
-        }
-
-        // 群AI已禁用
-        if (Objects.equals(pluginAi.getIsDisable(), 1)) {
-            botHelper.reply(event, "AI功能已关闭");
-            return;
-        }
+        PluginAi pluginAi = getAiOption(event, groupId);
+        if (pluginAi == null) return;
 
         // 获取当前群组的AI服务
         AiHandler aiHandler = aiHandlerSelector.get(groupId);
 
-        String text = getText(event.getArrayMsg());
-        text += ShiroUtils.getMsgImgUrlList(event.getArrayMsg()).stream().map(ocrUtil::baidu).collect(Collectors.joining(
-                "\n"));
-        log.info("问题：{} ", text);
-
-        // 不允许问题为空
-        if (StringUtils.isBlank(text)) {
-            botHelper.reply(event, "?");
-            return;
-        }
+        // 解析消息
+        String text = parseText(event);
+        if (text == null) return;
 
         ChatResponse response;
         if (text.startsWith("##")) {
             // 进行单次对话（不携带上下文，不会保留历史）
             response = aiHandler.generateAnswer(pluginAi, text);
-            log.info("AI： {}", response);
         } else {
-            // 当前用户的会话ID
-            String conversationId = groupId + "::" + event.getUserId();
-
-            try {
-                // 如果是回复机器人信息，则使用机器人所回复的用户的上下文
-                if (BotUtil.isReplyMe(event)) {
-                    ActionData<GetMsgResp> msgId =
-                            bot.getMsg(Integer.parseInt(event.getArrayMsg().stream().filter(it -> it.getType() == MsgTypeEnum.reply).findFirst().orElseThrow().getData().get("id")));
-                    String qq =
-                            BotUtil.parseArrayMsg(msgId.getData().getMessage()).stream().filter(it -> it.getType() == MsgTypeEnum.at).findFirst().orElseThrow().getData().get("qq");
-                    conversationId = groupId + "::" + qq;
-                    log.info("临时切换上下文：{} -> {}", event.getUserId(), qq);
-                }
-            } catch (Exception ignored) {
-            }
-
-
+            // 获取当前用户的会话ID
+            String conversationId = getConversationId(event, bot, groupId);
             // 清除当前用户上下文
             if (text.startsWith("!!") || text.startsWith("！！")) {
                 aiHandler.clear(conversationId);
                 text = text.substring(2);
             }
-
             // 生成AI回复
             response = aiHandler.generateAnswer(pluginAi, text, conversationId);
-            log.info("AI： {}", response);
         }
 
+        log.info("AI： {}", response);
+        // 发送消息
         botHelper.reply(event, response.getResult());
+
+        // TTS回复
         if (Objects.equals(pluginAi.getTts(), 1)) {
             String base64 = ttsUtil.generateToBase64(response.getResult());
             bot.sendGroupMsg(groupId, ArrayMsgUtils.builder().voice("base64://" + base64).build(), false);
@@ -124,6 +94,90 @@ public class AiPlugin {
         }
     }
 
+    /**
+     * 读取当前群组AI配置
+     */
+    private @Nullable PluginAi getAiOption(GroupMessageEvent event, Long groupId) {
+        PluginAi pluginAi = pluginAiRepository.findOne(groupId);
+        if (pluginAi == null) {
+            log.error("AI配置未初始化：{}", groupId);
+            return null;
+        }
+
+        // 群AI已禁用
+        if (Objects.equals(pluginAi.getIsDisable(), 1)) {
+            botHelper.reply(event, "AI功能已关闭");
+            return null;
+        }
+        return pluginAi;
+    }
+
+    /**
+     * 解析消息内容
+     * 1. 对图片进行OCR文本提取
+     * 2. 将``包裹的消息内容作为系统命令执行，并替换为执行结果
+     */
+    private @Nullable String parseText(GroupMessageEvent event) {
+        String text = getText(event.getArrayMsg());
+        Matcher execMatcher = Pattern.compile("`(.*?)`").matcher(text);
+
+        if (execMatcher.find()) {
+            String cmd = execMatcher.group(1);
+            try {
+                if (!botHelper.isSuperUser(event.getUserId()))
+                    throw new RuntimeException("非SU不能执行命令！");
+                text = CommandExecutor.execute(cmd, 10 * 1000);
+            } catch (Exception e) {
+                botHelper.reply(event, "命令执行失败：【%s】 => 【%s】".formatted(cmd, e.getMessage()));
+                return null;
+            }
+        }
+
+        text += ShiroUtils.getMsgImgUrlList(event.getArrayMsg())
+                .stream()
+                .map(ocrUtil::baidu)
+                .collect(Collectors.joining(
+                        "\n"));
+        log.info("问题：{} ", text);
+
+        // 不允许问题为空
+        if (StringUtils.isBlank(text)) {
+            botHelper.reply(event, "?");
+            return null;
+        }
+        return text;
+    }
+
+    /**
+     * 获取用户上下文标识
+     */
+    private static @NotNull String getConversationId(GroupMessageEvent event, Bot bot, Long groupId) {
+        // 当前用户的会话ID
+        String conversationId = groupId + "::" + event.getUserId();
+
+        try {
+            // 如果是回复机器人信息，则使用机器人所回复的用户的上下文
+            ActionData<GetMsgResp> msgId = bot.getMsg(Integer.parseInt(event.getArrayMsg()
+                    .stream()
+                    .filter(it -> it.getType() == MsgTypeEnum.reply)
+                    .findFirst()
+                    .orElseThrow()
+                    .getData()
+                    .get("id")));
+            String qq = BotUtil.parseArrayMsg(msgId.getData().getMessage())
+                    .stream()
+                    .filter(it -> it.getType() == MsgTypeEnum.at)
+                    .findFirst()
+                    .orElseThrow()
+                    .getData()
+                    .get("qq");
+            conversationId = event.getGroupId() + "::" + qq;
+            log.info("临时切换上下文：{} -> {}", event.getUserId(), qq);
+        } catch (Exception ignored) {
+        }
+        return conversationId;
+    }
+
     @Admin(mode = AdminMode.GROUP_ADMIN)
     @GroupMessageHandler
     @MessageHandlerFilter(startWith = ".ai", at = AtEnum.NOT_NEED)
@@ -131,7 +185,7 @@ public class AiPlugin {
 
         String usage = """
                 Usage:
-                - reset: 重置AI配置表
+                - reset: 重置AI配置表（仅SU可用）
                 - init: 初始化当前群组的AI配置
                 - disable: 禁用群组的AI功能
                 - enable: 启用群组的AI功能
@@ -153,8 +207,10 @@ public class AiPlugin {
         String action = split[0].toLowerCase();
         switch (action) {
             case "reset":
-                pluginAiRepository.initTable();
-                botHelper.reply(event, "初始化表成功");
+                if (botHelper.isSuperUser(event.getUserId())) {
+                    pluginAiRepository.initTable();
+                    botHelper.reply(event, "初始化表成功");
+                }
                 break;
             case "init":
                 AiHandler defaultAIHandler = aiHandlerSelector.getDefault();
